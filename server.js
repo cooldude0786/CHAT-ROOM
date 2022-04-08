140b538cf1f8436a3c9e541d800ded8cd47566f0
cooldude0786 <99258601+cooldude0786@users.noreply.github.com>
2022-04-08 17:59:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-console.log('hi')
-var name
-http.listen(4040, () => console.log("listening on http://localhost:4040"));
+var name , ListOfUser=[]
+http.listen(4040, /*'0.0.0.0',*/ () => console.log("listening on http://localhost:4040"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 io.on("connection", function(socket) {
 
 
-		socket.on("disconnect", function(socket) {
+		socket.on("disconnect", function(msg) {
 		// msg[1] = msg[1]+" Left the Chat"
 		io.emit("message", [false,name+' Left The Chat']);
-		console.log('User are Dead!'+socket);
+		console.log('Dead!'+msg[1]);
 	});
-
-
-
 		socket.on("message", function(msg) {
    		io.emit("message", msg);
   		console.log("useer send a message :"+msg)
  });
-
+		socket.on("dis",function(msg){
+			console.log(msg)
+		});
 
 
 	socket.on('joining msg', function(msg){
 		name=msg[1]
 		console.log("joing the chat")
+		ListOfUser.push(name)
 		msg[1] = msg[1]+" join the Chat"
 		io.emit("message", msg);
-		console.log(msg[1])
+		console.log(msg[1],ListOfUser)
+			// var UserArray = JSON.stringify(ListOfUser)
+			// localStorage.setItem("ActiveUser", UserArray )
+			// console.log("data after joining ",localStorage.getItem("ActiveUser"))
 	});
 
-
-
-});	
+});
