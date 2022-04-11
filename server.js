2b110f2ea62fbc931131508807c5227d7a0513e6
cooldude0786 <99258601+cooldude0786@users.noreply.github.com>
2022-04-11 18:06:48 +0530

    added d2d messag features

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-var name , ListOfUser=[]
+var name , ListOfUser=[],activeuser =[]
 http.listen(4040, /*'0.0.0.0',*/ () => console.log("listening on http://localhost:4040"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 io.on("connection", function(socket) {
 
 
 		socket.on("disconnect", function(msg) {
-		// msg[1] = msg[1]+" Left the Chat"
+		io.emit("dis","The User is Leaving")
+		ListOfUser=[]
 		io.emit("message", [false,name+' Left The Chat']);
 		console.log('Dead!'+msg[1]);
 	});
@@ -16,21 +17,30 @@ io.on("connection", function(socket) {
    		io.emit("message", msg);
   		console.log("useer send a message :"+msg)
  });
-		socket.on("dis",function(msg){
+
+		socket.on("peoplename",(msg)=>{
 			console.log(msg)
+			ListOfUser.push(msg)  
+			console.log("actice people",ListOfUser,ListOfUser.length)
+			io.emit("UserLoggIn",ListOfUser,activeuser) 
+			console.log("name in Active User array ",activeuser)
+			for(i=0; i < activeuser.length;i++){
+				if(activeuser.includes(ListOfUser[i]))
+				{
+					console.log("Found the user that leave the House",activeuser[i])
+				}
+			}
 		});
-
-
-	socket.on('joining msg', function(msg){
+		socket.on('joining msg', function(msg){
 		name=msg[1]
 		console.log("joing the chat")
 		ListOfUser.push(name)
+		activeuser.push(name)
 		msg[1] = msg[1]+" join the Chat"
 		io.emit("message", msg);
 		console.log(msg[1],ListOfUser)
-			// var UserArray = JSON.stringify(ListOfUser)
-			// localStorage.setItem("ActiveUser", UserArray )
-			// console.log("data after joining ",localStorage.getItem("ActiveUser"))
+		io.emit("UserLoggIn",ListOfUser)
 	});
 
-});
+}); 
+
