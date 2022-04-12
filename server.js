1711bdca973711d7a42302c58063e16cfb8cd5ef
cooldude0786 <99258601+cooldude0786@users.noreply.github.com>
2022-04-12 18:10:12 +0530

    Replace array with object

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-var name , ListOfUser=[],activeuser =[]
-http.listen(4040, /*'0.0.0.0',*/ () => console.log("listening on http://localhost:4040"));
+var name , ListOfUser=[]
+http.listen(4040, '0.0.0.0', () => console.log("listening on http://localhost:4040"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 io.on("connection", function(socket) {
 
@@ -10,7 +10,8 @@ io.on("connection", function(socket) {
 		socket.on("disconnect", function(msg) {
 		io.emit("dis","The User is Leaving")
 		ListOfUser=[]
-		io.emit("message", [false,name+' Left The Chat']);
+		io.emit("message",{Type : "Joining",
+							Message : "User Leave the Chat"});
 		console.log('Dead!'+msg[1]);
 	});
 		socket.on("message", function(msg) {
@@ -18,29 +19,22 @@ io.on("connection", function(socket) {
   		console.log("useer send a message :"+msg)
  });
 
-		socket.on("peoplename",(msg)=>{
-			console.log(msg)
+		socket.on("peoplename",async (msg)=>{
+			console.log("msg:= ",msg)
 			ListOfUser.push(msg)  
-			console.log("actice people",ListOfUser,ListOfUser.length)
-			io.emit("UserLoggIn",ListOfUser,activeuser) 
-			console.log("name in Active User array ",activeuser)
-			for(i=0; i < activeuser.length;i++){
-				if(activeuser.includes(ListOfUser[i]))
-				{
-					console.log("Found the user that leave the House",activeuser[i])
-				}
-			}
+			console.log("active people in ListOfUser array:=",ListOfUser,ListOfUser.length)
+			await io.emit("UserLoggIn",ListOfUser) 
 		});
 		socket.on('joining msg', function(msg){
 		name=msg[1]
 		console.log("joing the chat")
 		ListOfUser.push(name)
-		activeuser.push(name)
 		msg[1] = msg[1]+" join the Chat"
-		io.emit("message", msg);
+		io.emit("message", {Type : "Joining",
+							Name : name,
+							Message : name+" join the Chat"});
 		console.log(msg[1],ListOfUser)
 		io.emit("UserLoggIn",ListOfUser)
 	});
-
-}); 
-
+ 
+});   
